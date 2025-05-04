f49dcdb2038c7557ef044d57cf192f5b6e7b5303
Leonel Contreras <lecontreras@frba.utn.edu.ar>
2025-05-04 15:21:15 -0300

    Fix: 13

--- GUIA SQL/13.sql
+++ GUIA SQL/13.sql
@@ -4,9 +4,9 @@ de los productos que lo componen. Solo se deberán mostrar los productos que est
 compuestos por más de 2 productos y deben ser ordenados de mayor a menor por
 cantidad de productos que lo componen.*/
 
-SELECT prod_codigo, prod_detalle, (SELECT SUM(isnull(prod_precio, 0)) * comp_cantidad
-								   FROM Producto
-								   WHERE comp_componente = prod_codigo)
-FROM Producto JOIN Composicion ON prod_codigo = comp_producto
-WHERE comp_cantidad > 2
-ORDER BY comp_cantidad DESC
+SELECT Combo.prod_codigo, Combo.prod_detalle, Combo.prod_precio, SUM(isnull(Comp.prod_precio * comp_cantidad, 0)) 
+FROM Producto Combo JOIN Composicion ON Combo.prod_codigo = comp_producto
+JOIN Producto Comp ON Comp.prod_codigo = comp_componente
+GROUP BY Combo.prod_codigo, Combo.prod_detalle, Combo.prod_precio
+HAVING COUNT(comp_componente) > 2
+ORDER BY COUNT(comp_componente) DESC
