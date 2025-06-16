ce1185b619784466a3cb7ea1e69278ab50e7d15a
Leonel Contreras <lecontreras@frba.utn.edu.ar>
2025-06-15 21:43:21 -0300

    10 v3

--- GUIA TSQL/10.sql
+++ GUIA TSQL/10.sql
@@ -2,6 +2,19 @@
 verifique que no exista stock y si es así lo borre en caso contrario que emita un
 mensaje de error.*/
 -- No se usa cursores porque éstos solo son necesarios para hacer validaciones o logica especifica para muchas filas, para operaciones CRUD son innecesarios
+
+
+CREATE TRIGGER ej10 ON Producto AFTER DELETE
+AS
+BEGIN
+	IF(SELECT COUNT(*) FROM deleted JOIN Stock ON stoc_producto = prod_codigo AND stoc_cantidad > 0) > 0
+	BEGIN
+		ROLLBACK
+		RAISERROR('Operación inválida')
+	END
+END
+GO
+
 CREATE TRIGGER ej10 ON Producto INSTEAD OF DELETE
 AS
 BEGIN
