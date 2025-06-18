2c0861f6602830303062844a9d999af80b7b9920
Leonel Contreras <lecontreras@frba.utn.edu.ar>
2025-06-18 15:45:41 -0300

    21 corregido

--- GUIA TSQL/21.sql
+++ GUIA TSQL/21.sql
@@ -12,17 +12,6 @@ BEGIN
 			   GROUP BY item_tipo, item_sucursal, item_numero
 			   HAVING COUNT(DISTINCT prod_familia) > 1)
 	BEGIN
-		DELETE FROM Factura
-		WHERE fact_tipo+fact_sucursal+fact_numero IN (SELECT item_tipo+item_sucursal+item_numero
-													  FROM inserted
-													  JOIN Producto ON item_producto = prod_codigo
-													  GROUP BY item_tipo+item_sucursal+item_numero
-													  HAVING COUNT(DISTINCT prod_familia) > 1)
-		PRINT 'No puede comprar items de diferentes familias'
-	END
-
-	ELSE
-	BEGIN
 		INSERT INTO Item_Factura (item_tipo, item_sucursal, item_numero, item_producto, item_cantidad, item_precio)
 		SELECT item_tipo, item_sucursal, item_numero, item_producto, item_cantidad, item_precio 
 		FROM inserted
@@ -31,26 +20,30 @@ BEGIN
 														  JOIN Producto ON item_producto = prod_codigo
 														  GROUP BY item_tipo+item_sucursal+item_numero
 														  HAVING COUNT(DISTINCT prod_familia) > 1)
+
+		DELETE FROM Factura
+		WHERE fact_tipo+fact_sucursal+fact_numero IN (SELECT item_tipo+item_sucursal+item_numero
+													  FROM inserted
+													  JOIN Producto ON item_producto = prod_codigo
+													  GROUP BY item_tipo+item_sucursal+item_numero
+													  HAVING COUNT(DISTINCT prod_familia) > 1)
+		PRINT 'No puede comprar items de diferentes familias'
 	END
 END
 
 BEGIN TRANSACTION
 INSERT INTO Factura (fact_tipo, fact_sucursal, fact_numero, fact_fecha, fact_vendedor, fact_total, fact_total_impuestos, fact_cliente)
-VALUES ('A', '0003', 'FFFFFFF1', GETDATE(), 4, 100, 0, '01634')
+VALUES ('A', '0003', 'FFFFFFF1', GETDATE(), 4, 100, 0, '01634'),
+       ('A', '0003', 'FFFFFFF2', GETDATE(), 4, 100, 0, '01634')
 
 INSERT INTO Item_Factura (item_tipo, item_sucursal, item_numero, item_producto, item_cantidad, item_precio)
 VALUES ('A', '0003', 'FFFFFFF1', '00000030', 1, 1),
-	   ('A', '0003', 'FFFFFFF1', '00000123', 1, 1)
+	   ('A', '0003', 'FFFFFFF1', '00000123', 1, 1),
+	   ('A', '0003', 'FFFFFFF2', '00000123', 1, 1)
 
 ROLLBACK
 
 SELECT * FROM Factura WHERE fact_numero = 'FFFFFFF1'
 SELECT * FROM Item_Factura WHERE item_numero = 'FFFFFFF1'
 SELECT * FROM Producto WHERE prod_codigo = '00000030'
-
-SELECT 1 
-			   FROM Item_Factura
-			   JOIN Producto ON item_producto = prod_codigo
-			   GROUP BY item_tipo+item_sucursal+item_numero
-			   HAVING COUNT(DISTINCT prod_familia) > 1
 
