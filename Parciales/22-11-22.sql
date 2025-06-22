5e6820d4e035faabeee71293b66a40be1d1d4e42
Leonel Contreras <lecontreras@frba.utn.edu.ar>
2025-06-22 11:59:49 -0300

    22-11-22 v2

--- Parciales/22-11-22.sql
+++ Parciales/22-11-22.sql
@@ -1,35 +1,27 @@
 --- SQL ---
-SELECT P.prod_codigo,
-	   P.prod_detalle,
+SELECT P1.prod_codigo,
+       P1.prod_detalle,
 	  (SELECT SUM(ISNULL(item_cantidad, 0))
-	   FROM Item_Factura 
-	   JOIN Factura ON item_tipo = fact_tipo 
-			AND item_sucursal = fact_sucursal 
-			AND item_numero = fact_numero 
-			AND YEAR(fact_fecha) = 2012
-	   WHERE item_producto IN (SELECT comp_componente
-							   FROM Composicion
-							   WHERE comp_producto = P.prod_codigo)),
-	   SUM(ISNULL(item_cantidad, 0) * ISNULL(item_precio, 0))
-FROM Producto P
-JOIN Item_Factura ON item_producto = P.prod_codigo
-JOIN Factura ON item_tipo = fact_tipo AND item_sucursal = fact_sucursal AND item_numero = fact_numero
-WHERE P.prod_codigo IN (SELECT P1.prod_codigo
-						FROM Producto P1
-						JOIN Composicion ON comp_producto = P1.prod_codigo
-						JOIN Producto P2 ON comp_componente = P2.prod_codigo
-						GROUP BY P1.prod_codigo
-						HAVING COUNT(comp_componente) = 3 AND COUNT(DISTINCT P2.prod_rubro) = 2)
-GROUP BY P.prod_codigo, P.prod_detalle
-ORDER BY (SELECT COUNT(DISTINCT fact_numero+fact_tipo+fact_sucursal)
-	   FROM Item_Factura 
-	   JOIN Factura ON item_tipo = fact_tipo 
-			AND item_sucursal = fact_sucursal 
-			AND item_numero = fact_numero 
-			AND YEAR(fact_fecha) = 2012
-	   WHERE item_producto IN (SELECT comp_componente
-							   FROM Composicion
-							   WHERE comp_producto = P.prod_codigo)) DESC
+	   FROM Item_Factura
+	   JOIN Factura ON fact_tipo = item_tipo AND fact_numero = item_numero AND fact_sucursal = item_sucursal
+	   WHERE YEAR(fact_fecha) = 2012 AND item_producto IN (SELECT comp_componente
+														   FROM Composicion
+														   WHERE comp_producto = P1.prod_codigo)) AS cant_ventas_componentes,
+	  (SELECT SUM(ISNULL(item_cantidad, 0) * ISNULL(item_cantidad, 0))
+	   FROM Item_Factura
+	   WHERE item_producto = P1.prod_codigo) AS monto_total
+FROM Producto P1
+WHERE P1.prod_codigo IN (SELECT comp_producto
+					  FROM Composicion
+					  JOIN Producto PP ON PP.prod_codigo = comp_componente
+					  GROUP BY comp_producto
+					  HAVING COUNT(comp_componente) = 3 AND COUNT(DISTINCT PP.prod_rubro) = 2)
+ORDER BY (SELECT COUNT(DISTINCT item_tipo+item_sucursal+item_numero)
+		  FROM Item_Factura
+		  JOIN Factura ON fact_tipo = item_tipo AND fact_numero = item_numero AND fact_sucursal = item_sucursal
+		  WHERE YEAR(fact_fecha) = 2012 AND item_producto IN (SELECT comp_componente
+															  FROM Composicion
+														      WHERE comp_producto = P1.prod_codigo))
 
 
 --- TSQL ---
